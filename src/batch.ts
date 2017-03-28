8c26ddd039a9b021adc77548ee936a3ab6cd5fb4
Federico Pinna <fpinna@vivocha.com>
2017-03-28 16:06:38 +0200

    Initial commit

---
+++ src/batch.ts
@@ -0,0 +1,158 @@
+const debug = require('debug')('loadtest');
+
+export interface TestModule {
+  (...args: any[]): Promise<Test>;
+}
+
+export interface Test {
+  dispose?: () => Promise<any>;
+  beforeEach?: () => Promise<any>;
+  afterEach?: () => Promise<any>;
+  run: () => Promise<any>;
+}
+
+export class BatchOptions {
+  test: string;
+  timeout?: number;
+  iterations?: number;
+  concurrent?: number;
+  args?: any[];
+}
+
+export class BatchResults {
+  executed: number;
+  succeeded: number;
+  failed: number;
+  totDuration: number;
+  minDuration: number;
+  maxDuration: number;
+  avgDuration: number;
+  results: any[];
+  errors: any[];
+}
+
+export class BatchProgress {
+  executed: number;
+  succeeded: number;
+  failed: number;
+  totDuration: number;
+  curDuration: number;
+  result?: any;
+  error?: any;
+}
+
+export class Batch {
+  readonly module:TestModule;
+
+  constructor(protected opts: BatchOptions) {
+    if (typeof opts.timeout !== 'number' || opts.timeout < 0) {
+      opts.timeout = 20000;
+    }
+    if (typeof opts.iterations !== 'number' || opts.iterations <= 0) {
+      opts.iterations = 1;
+    }
+    if (typeof opts.concurrent !== 'number' || opts.concurrent <= 0) {
+      opts.concurrent = 1;
+    }
+    this.module = require(this.opts.test) as TestModule;
+  }
+  run(progress?:(info:BatchProgress) => void): Promise<BatchResults> {
+    let results: BatchResults = {
+      executed: 0,
+      succeeded: 0,
+      failed: 0,
+      totDuration: 0,
+      minDuration: 0,
+      maxDuration: 0,
+      avgDuration: 0,
+      results: [],
+      errors: []
+    };
+    return this.module(...(this.opts.args || [])).then((test: Test) => {
+      return new Promise(resolve => {
+        let runnersLeft = this.opts.iterations;
+        let runnersActive = 0;
+        let runner = (): Promise<any> => {
+          if (!runnersLeft) {
+            results.avgDuration = results.totDuration / results.executed;
+            return Promise.resolve();
+          } else {
+            runnersLeft--;
+            runnersActive++;
+            debug(`starting new runner, left ${runnersLeft}, active ${runnersActive}`);
+            let r;
+            if (test.beforeEach) {
+              r = test.beforeEach();
+            } else {
+              r = Promise.resolve();
+            }
+            return r.then(() => new Promise((resolve, reject) => {
+              let start = new Date();
+              let t = setTimeout(() => reject(new Error('timeout')), this.opts.timeout);
+              let info:BatchProgress = {
+                executed: 0,
+                succeeded: 0,
+                failed: 0,
+                totDuration: 0,
+                curDuration: 0
+              };
+              test.run().then(result => {
+                info.result = result;
+                results.succeeded++;
+                results.results.push(result);
+              }, err => {
+                info.error = err;
+                results.failed++;
+                results.results.push(err);
+              }).then(() => {
+                clearTimeout(t);
+                runnersActive--;
+                results.executed++;
+                let end = new Date();
+                let dur = +end - +start;
+                debug(`completed runner in ${dur / 1000}s, left ${runnersLeft}, active ${runnersActive}`);
+                results.totDuration += dur;
+                if (!results.minDuration || dur < results.minDuration) {
+                  results.minDuration = dur;
+                }
+                if (!results.maxDuration || dur > results.maxDuration) {
+                  results.maxDuration = dur;
+                }
+                if (progress) {
+                  info.executed = results.executed;
+                  info.succeeded = results.succeeded;
+                  info.failed = results.failed;
+                  info.totDuration = results.totDuration;
+                  info.curDuration = dur;
+                  progress(info);
+                }
+              }).then(resolve, reject);
+            }).then(() => {
+              if (test.afterEach) {
+                return test.afterEach();
+              }
+            })).then(runner);
+          }
+        };
+        let concurrent = this.opts.concurrent || 1;
+        if (concurrent === 1) {
+          resolve(runner());
+        } else {
+          let runners: Promise<any>[] = [];
+          for (let i = 0 ; i < concurrent ; i++) {
+            runners.push(runner());
+          }
+          resolve(Promise.all(runners));
+        }
+      }).then(() => {}, err => {
+        results.errors.push(err);
+      }).then(() => {
+        if (test.dispose) {
+          return test.dispose();
+        }
+      });
+    }).then(() => {}, err => {
+      results.errors.push(err);
+    }).then(() => results);
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+#!/bin/env node
+
+import { Batch } from './batch';
+
+const program = require('commander');
+
+program
+  .version('1.0.0')
+  .usage('[options] <module> -- [module args]')
+  .option('-i, --iterations <n>', 'Number of tests to execute (default 1)', parseInt, 1)
+  .option('-c, --concurrent <n>', 'Number of concurrent tests (default 1)', parseInt, 1)
+  .option('-t, --timeout <ms>', 'Timeout per single test execution (default 2000)', parseInt, 2000)
+  .parse(process.argv);
+
+if (program.args.length < 1) {
+  program.help();
+}
+
+let b = new Batch({
+  test: process.cwd() + '/' + program.args[0],
+  iterations: program.iterations || 1,
+  concurrent: program.concurrent || 1,
+  timeout: program.timeout || 2000,
+  args: program.args.length > 1 ? program.args.slice(1) : []
+});
+
+b.run(data => {
+  if (data.error) {
+    process.stdout.write('x');
+  } else {
+    process.stdout.write('.');
+  }
+}).then(results => {
+  process.stdout.write('\n');
+  console.log(results);
+  if (results.failed == 0) {
+    process.exit(0);
+  } else {
+    process.exit(1);
+  }
+});
